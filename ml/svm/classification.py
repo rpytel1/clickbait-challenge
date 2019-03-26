9169088013f46b6b618f69825af314540e7e83b8
RafailSkoulos17 <rafail.sk17@hotmail.gr>
2019-03-26 21:19:20 +0100

    Added a simple implementation of SVM. Needs tuning.

---
+++ ml/svm/classification.py
@@ -0,0 +1,58 @@
+import pprint
+import numpy as np
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, confusion_matrix
+from sklearn import svm
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import StratifiedShuffleSplit, KFold
+
+from ml.svm.get_features_and_labels import get_features_and_labels
+
+X, y = get_features_and_labels()
+sss = StratifiedShuffleSplit(n_splits=10, random_state=42)
+
+accuracy = 0
+precision = 0
+recall = 0
+f1 = 0
+f_scores = []
+
+for train_index, test_index in sss.split(X, y):
+    X_train, X_test = X[train_index], X[test_index]
+    y_train, y_test = y[train_index], y[test_index]
+
+    # std_scale = StandardScaler().fit(X_train)
+    # X_train = std_scale.transform(X_train)
+    # X_test = std_scale.transform(X_test)
+
+    clf = svm.SVC(kernel='rbf', C=198.85295716582226, gamma=0.059683872490462968, class_weight='balanced',
+                  random_state=42)
+    clf.fit(X_train, y_train)
+    y_pred = clf.predict(X_test)
+
+    accuracy += accuracy_score(y_test, y_pred) * 100
+
+    tn, fp, fn, tp = conf_matrix = confusion_matrix(y_test, y_pred).ravel()
+    print("(TN, FP, FN, TP) = {}".format((tn, fp, fn, tp)))
+
+    # RECALL = tp / (tp + fn)
+    # PRECISION = tp / (tp + fp)
+    # F1 = 2 * (PRECISION * RECALL) / (PRECISION + RECALL)
+
+    precision += precision_score(y_test, y_pred) * 100
+    recall += recall_score(y_test, y_pred) * 100
+    f_scores.append(f1_score(y_test, y_pred) * 100)
+    f1 += f1_score(y_test, y_pred) * 100
+
+print("Accuracy is ", accuracy / 10)
+
+print("Precision is", precision / 10)
+
+print("Recall is", recall / 10)
+
+print("F1-core is ", f1 / 10)
+
+f_scores = np.array(f_scores)
+v = (np.var(f_scores) ** (0.5)) * 2
+
+print("Variance is: {0}".format(v))
+pprint.pprint(f_scores)
---
+++ ml/svm/get_features_and_labels.py
@@ -0,0 +1,41 @@
+import json
+
+import jsonpickle
+import pandas as pd
+
+
+def get_features_and_labels():
+
+    filename = "../../data_reading/data.txt"
+    with open(filename) as json_file:
+        data_encoded = json.load(json_file)
+        data = jsonpickle.decode(data_encoded)
+
+    post_dict = {}
+    for post in data:
+        post_dict[post.id] = {}
+        for ind, feature in enumerate(post.features):
+            post_dict[post.id][ind] = feature
+
+    truth_file = '../../data/clickbait-training/truth.jsonl'
+    with open(truth_file, encoding="utf-8") as f:
+        for ind, line in enumerate(f):
+            post = json.loads(line)
+            if post["truthClass"] == "no-clickbait":
+                clickbait = 1
+            else:
+                clickbait = 0
+            post_dict[post["id"]]["label"] = clickbait
+
+    index = post_dict.keys()
+    features = pd.DataFrame.from_dict(post_dict, orient='index', dtype=None)
+
+    labels_df = features.loc[:, 'label']
+    features_df = features.drop('label', 1)
+
+    X = features_df.values
+    y = labels_df.values
+    return X, y
+
+
+
