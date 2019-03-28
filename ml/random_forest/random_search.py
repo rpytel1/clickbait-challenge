1080853819fa5db0afa0e9ff097179719b41c2f6
RafailSkoulos17 <rafail.sk17@hotmail.gr>
2019-03-28 19:25:00 +0100

    Added Random Forest regressor implementation.

---
+++ ml/random_forest/random_search.py
@@ -0,0 +1,47 @@
+import numpy as np
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import make_scorer, mean_squared_error
+from sklearn.model_selection import RandomizedSearchCV
+from feature_extraction.services.utils.regression_features_and_labels import get_features_and_labels
+
+
+def rf_randomized_search(X, y):
+    mse_scorer = make_scorer(mean_squared_error)
+
+    # scaler = StandardScaler().fit(X)
+    # X = scaler.transform(X)
+
+    # Number of trees in random forest
+    n_estimators = [int(x) for x in np.linspace(start=200, stop=2000, num=10)]
+    # Number of features to consider at every split
+    max_features = ['auto', 'sqrt']
+    # Maximum number of levels in tree
+    max_depth = [int(x) for x in np.linspace(10, 110, num=11)]
+    max_depth.append(None)
+    # Minimum number of samples required to split a node
+    min_samples_split = [2, 5, 10]
+    # Minimum number of samples required at each leaf node
+    min_samples_leaf = [1, 2, 4]
+    # Method of selecting samples for training each tree
+    bootstrap = [True, False]
+    # Create the random grid
+    random_grid = {'n_estimators': n_estimators,
+                   'max_features': max_features,
+                   'max_depth': max_depth,
+                   'min_samples_split': min_samples_split,
+                   'min_samples_leaf': min_samples_leaf,
+                   'bootstrap': bootstrap}
+
+    n_iter_search = 10
+    random_search = RandomizedSearchCV(RandomForestRegressor(), param_distributions=random_grid,
+                                       n_iter=n_iter_search,
+                                       scoring={'mse': mse_scorer},
+                                       cv=3, refit='mse', verbose=2, n_jobs=-1, random_state=42)
+
+    random_search.fit(X, y)
+    print(random_search.best_params_)
+
+
+if __name__ == "__main__":
+    X, _, truthMean = get_features_and_labels()
+    rf_randomized_search(X, truthMean)
