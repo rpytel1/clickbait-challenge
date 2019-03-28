0dd7a7f755fec26c2f091b05d402f8ea38720d7d
LENOVO-PC\Vasilis <el12021@central.ntua.gr>
2019-03-28 22:00:50 +0100

    Behaviour and time features added

--- feature_extraction/services/behaviour_analysis_service.py
+++ feature_extraction/services/behaviour_analysis_service.py
@@ -1,4 +1,5 @@
 from feature_extraction.services.image_service import get_text_from_image
+import nltk
 
 
 def calculate_all_behaviour_features(entry):
@@ -15,22 +16,23 @@ def calculate_all_behaviour_features(entry):
 
 
 def get_feat_names():
-    behaviour_post_title_feats = ['@_in_post_title', '!_in_post_title', '#_in_post_title', '?_in_post_title']
-    behaviour_post_image_feats = ['@_in_post_image', '!_in_post_image', '#_in_post_image', '?_in_post_image']
+    behaviour_post_title_feats = ['@_in_post_title', '!_in_post_title', '#_in_post_title', '?_in_post_title', 'links_in_post_title']
+    behaviour_post_image_feats = ['@_in_post_image', '!_in_post_image', '#_in_post_image', '?_in_post_image', 'links_in_post_image']
     behaviour_article_title_feats = ['@_in_article_title', '!_in_article_title', '#_in_article_title',
-                                     '?_in_article_title']
-    behaviour_article_desc_feats = ['@_in_article_desc', '!_in_article_desc', '#_in_article_desc', '?_in_article_desc']
+                                     '?_in_article_title', 'links_in_article_title']
+    behaviour_article_desc_feats = ['@_in_article_desc', '!_in_article_desc', '#_in_article_desc', '?_in_article_desc', 'links_in_article_desc']
     behaviour_article_keywords_feats = ['@_in_article_keywords', '!_in_article_keywords', '#_in_article_keywords',
-                                        '?_in_article_keywords']
+                                        '?_in_article_keywords', 'links_in_article_keywords']
     behaviour_article_captions_feats = ['@_in_article_captions', '!_in_article_captions', '#_in_article_captions',
-                                        '?_in_article_captions']
+                                        '?_in_article_captions', 'links_in_article_captions']
     behaviour_article_paragraphs_feats = ['@_in_article_paragraphs', '!_in_article_paragraphs',
-                                          '#_in_article_paragraphs', '?_in_article_paragraphs']
+                                          '#_in_article_paragraphs', '?_in_article_paragraphs', 'links_in_article_paragraphs']
     return behaviour_post_title_feats + behaviour_post_image_feats + behaviour_article_title_feats + \
            behaviour_article_desc_feats + behaviour_article_keywords_feats + behaviour_article_captions_feats + \
            behaviour_article_paragraphs_feats
 
 
 def calculate_special_signs(text):
-    # Calculate no of occurnces of @ ! # and ?
-    return text.count("@"), text.count("!"), text.count("#"), text.count("?")
+    # Calculate no of occurrences of @ ! # and ? and links
+    tokenizer = nltk.RegexpTokenizer(r'https?://(?:[-\w./.]|(?:%[\da-fA-F]{2}))+')
+    return text.count("@"), text.count("!"), text.count("#"), text.count("?"), len(tokenizer.tokenize(text))
--- feature_extraction/services/time_service.py
+++ feature_extraction/services/time_service.py
@@ -5,10 +5,12 @@ import numpy as np
 
 def calculate_time_features(entry):
     dt = datetime.datetime.strptime(entry["postTimestamp"], '%a %b %d %H:%M:%S %z %Y')
-
     hours = int(np.floor(dt.timestamp() / 3600))
-    return hours
+    weekend = 1
+    if 0 <= datetime.date(dt.year, dt.month, dt.day).weekday() <= 4:
+        weekend = 0
+    return hours, weekend, int(np.round(dt.hour+dt.minute/60))
 
 
 def get_feat_names():
-    return "post_creation_hour"
+    return "post_creation_hour", "is_weekend", "hour_of_day"
