8f78e4f2e9f795cc817f9f875610398b1811a1ad
LENOVO-PC\Vasilis <el12021@central.ntua.gr>
2019-03-30 15:55:03 +0100

    Contraction feature added

--- feature_extraction/services/behaviour_analysis_service.py
+++ feature_extraction/services/behaviour_analysis_service.py
@@ -5,6 +5,128 @@ from nltk.corpus import stopwords
 import re
 
 
+cList = [
+  "ain't",
+  "aren't",
+  "can't",
+  "can't've",
+  "'cause",
+  "could've",
+  "couldn't",
+  "couldn't've",
+  "didn't",
+  "doesn't",
+  "don't",
+  "hadn't",
+  "hadn't've",
+  "hasn't",
+  "haven't",
+  "he'd",
+  "he'd've",
+  "he'll",
+  "he'll've",
+  "he's",
+  "how'd",
+  "how'd'y",
+  "how'll",
+  "how's",
+  "I'd",
+  "I'd've",
+  "I'll",
+  "I'll've",
+  "I'm",
+  "I've",
+  "isn't",
+  "it'd",
+  "it'd've",
+  "it'll",
+  "it'll've",
+  "it's",
+  "let's",
+  "ma'am",
+  "mayn't",
+  "might've",
+  "mightn't",
+  "mightn't've",
+  "must've",
+  "mustn't",
+  "mustn't've",
+  "needn't",
+  "needn't've",
+  "o'clock",
+  "oughtn't",
+  "oughtn't've",
+  "shan't",
+  "sha'n't",
+  "shan't've",
+  "she'd",
+  "she'd've",
+  "she'll",
+  "she'll've",
+  "she's",
+  "should've",
+  "shouldn't",
+  "shouldn't've",
+  "so've",
+  "so's",
+  "that'd",
+  "that'd've",
+  "that's",
+  "there'd",
+  "there'd've",
+  "there's",
+  "they'd",
+  "they'd've",
+  "they'll",
+  "they'll've",
+  "they're",
+  "they've",
+  "to've",
+  "wasn't",
+  "we'd",
+  "we'd've",
+  "we'll",
+  "we'll've",
+  "we're",
+  "we've",
+  "weren't",
+  "what'll",
+  "what'll've",
+  "what're",
+  "what's",
+  "what've",
+  "when's",
+  "when've",
+  "where'd",
+  "where's",
+  "where've",
+  "who'll",
+  "who'll've",
+  "who's",
+  "who've",
+  "why's",
+  "why've",
+  "will've",
+  "won't",
+  "won't've",
+  "would've",
+  "wouldn't",
+  "wouldn't've",
+  "y'all",
+  "y'alls",
+  "y'all'd",
+  "y'all'd've",
+  "y'all're",
+  "y'all've",
+  "you'd",
+  "you'd've",
+  "you'll",
+  "you'll've",
+  "you're",
+  "you've"
+]
+
+
 def calculate_all_behaviour_features(entry):
     behaviour_post_title = calculate_special_signs(entry["postText"][0])
     behaviour_post_image = calculate_special_signs(get_text_from_image(entry))
@@ -25,24 +147,31 @@ def calculate_all_behaviour_features(entry):
 
 def get_feat_names():
     behaviour_post_title_feats = ['@_in_post_title', '!_in_post_title', '#_in_post_title', '?_in_post_title',
-                                  'links_in_post_title', 'avg_word_len_post_title', 'fr_stop_words_post_title']
+                                  '*_in_post_title', 'links_in_post_title', 'avg_word_len_post_title',
+                                  'fr_stop_words_post_title', 'contractions_in_post_title']
     behaviour_post_image_feats = ['@_in_post_image', '!_in_post_image', '#_in_post_image', '?_in_post_image',
-                                  'links_in_post_image', 'avg_word_len_post_image', 'fr_stop_words_post_image']
+                                  '*_in_post_image', 'links_in_post_image', 'avg_word_len_post_image',
+                                  'fr_stop_words_post_image', 'contractions_in_post_image']
     behaviour_article_title_feats = ['@_in_article_title', '!_in_article_title', '#_in_article_title',
-                                     '?_in_article_title', 'links_in_article_title', 'avg_word_len_article_title',
-                                     'fr_stop_words_article_title']
+                                     '?_in_article_title', '*_in_article_title', 'links_in_article_title',
+                                     'avg_word_len_article_title', 'fr_stop_words_article_title',
+                                     'contractions_in_article_title']
     behaviour_article_desc_feats = ['@_in_article_desc', '!_in_article_desc', '#_in_article_desc', '?_in_article_desc',
-                                    'links_in_article_desc', 'avg_word_len_article_desc', 'fr_stop_words_article_desc']
+                                    '*_in_article_desc', 'links_in_article_desc', 'avg_word_len_article_desc',
+                                    'fr_stop_words_article_desc', 'contractions_in_article_desc']
     behaviour_article_keywords_feats = ['@_in_article_keywords', '!_in_article_keywords', '#_in_article_keywords',
-                                        '?_in_article_keywords', 'links_in_article_keywords',
-                                        'avg_word_len_article_keywords', 'fr_stop_words_article_keywords']
+                                        '?_in_article_keywords', '*_in_article_keywords', 'links_in_article_keywords',
+                                        'avg_word_len_article_keywords', 'fr_stop_words_article_keywords',
+                                        'contractions_in_article_keywords']
     behaviour_article_captions_feats = ['@_in_article_captions', '!_in_article_captions', '#_in_article_captions',
-                                        '?_in_article_captions', 'links_in_article_captions',
-                                        'avg_word_len_article_captions', 'fr_stop_words_article_captions']
+                                        '?_in_article_captions', '*_in_article_captions', 'links_in_article_captions',
+                                        'avg_word_len_article_captions', 'fr_stop_words_article_captions',
+                                        'contractions_in_article_captions']
     behaviour_article_paragraphs_feats = ['@_in_article_paragraphs', '!_in_article_paragraphs',
                                           '#_in_article_paragraphs', '?_in_article_paragraphs',
-                                          'links_in_article_paragraphs', 'avg_word_len_article_paragraphs',
-                                          'fr_stop_words_article_paragraphs']
+                                          '*_in_article_paragraphs', 'links_in_article_paragraphs',
+                                          'avg_word_len_article_paragraphs', 'fr_stop_words_article_paragraphs',
+                                          'contractions_in_article_paragraphs']
     return behaviour_post_title_feats + behaviour_post_image_feats + behaviour_article_title_feats + \
            behaviour_article_desc_feats + behaviour_article_keywords_feats + behaviour_article_captions_feats + \
            behaviour_article_paragraphs_feats + ['post_title_starts_num', 'article_title_starts_num',
@@ -53,11 +182,12 @@ def calculate_special_signs(text):
     # Calculate no of occurrences of @ ! # and ? and links, avg word length and fraction of stopwords
     tokenizer1 = nltk.RegexpTokenizer(r'https?://(?:[-\w./.]|(?:%[\da-fA-F]{2}))+')
     tokenizer2 = nltk.RegexpTokenizer(r'\w+')  # TODO: check the appropriateness of such regexp
-    return [text.count("@"), text.count("!"), text.count("#"), text.count("?"), len(tokenizer1.tokenize(text)),
+    return [text.count("@"), text.count("!"), text.count("#"), text.count("?"), text.count("*"),
+            len(tokenizer1.tokenize(text)),
            0 if not list(map(lambda x: len(x), tokenizer2.tokenize(text))) else reduce((lambda x, y: x + y), list(
                map(lambda x: len(x), tokenizer2.tokenize(text)))) / len(tokenizer2.tokenize(text)),
            0 if not len(tokenizer2.tokenize(text)) else len([w for w in tokenizer2.tokenize(text) if w.lower()
-                in stopwords.words('english')]) / len(tokenizer2.tokenize(text))]
+                in stopwords.words('english')]) / len(tokenizer2.tokenize(text)), check_contractions(text)]
 
 
 def check_num(text):
@@ -66,3 +196,7 @@ def check_num(text):
 
 def check_5w1h(text):
     return 0 if not re.search(r'^\s*(?:who|why|what|when|where|how)', text.lower()) else 1
+
+
+def check_contractions(text):
+    return sum(text.lower().count(c) for c in cList)
